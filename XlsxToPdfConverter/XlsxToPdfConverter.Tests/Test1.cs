cb993db625efc494b66dead2b6bc26c4c2e5e79e
GNIVC\MarkovaDM <Markovadm@gnivc.ru>
2026-06-19 10:26:26 +0300

    add xlxs to pdf converter tests

---
+++ XlsxToPdfConverter/XlsxToPdfConverter.Tests/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace XlsxToPdfConverter.Tests
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
